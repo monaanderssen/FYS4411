ab1c969967a69684980609efb7ac2e572f081a22
jonasron <46788041+jonasron@users.noreply.github.com>
2019-01-31 15:01:18 +0100

    Add files via upload

--- Project1/Code/harmonicocillator.cpp
+++ Project1/Code/harmonicocillator.cpp
@@ -1,15 +1,45 @@
 #include "harmonicocillator.h"
 #include "wavefunction.h"
+#include <vector>
+
+using std::vector;
+
+void harmonicocillator::setPsi() {
+	psi.setDimensions(dimension);
+	psi.setNumberOfParticels(numberOfParticles);
+	psi.setParticels();
+}
+
+
 
 double harmonicocillator::harmonicOcillatorWavefunction() {
-	double product = 1;
-	for (int i = 0; i < psi.getNumberOfParticles(); i++) {
-		double sum = 0;
+	double product = 1.0;
+	int dimension = psi.getDimensions();
+	int N = psi.getNumberOfParticles();
+	for (int i = 0; i < N; i++) {
+		double sum = 0.0;
 		vector<double> temp = psi.getParticle(i).getPosition();
-		for (int j = 0; j < psi.getDimensions(); j++) {
+		for (int j = 0; j < dimension; j++) {
 			sum += temp[j] * temp[j];
 		}
 		product *= exp(-alpha * sum);
 	}
 	return product;
+}
+
+
+double harmonicocillator::localEnergy() {
+	double sum = 0.0;
+	int dimension = psi.getDimensions();
+	int N = psi.getNumberOfParticles();
+	for (int i = 0; i < N; i++) {
+		vector<double> temp = psi.getParticle(i).getPosition();
+		double tempSum = 0.0;
+		for (int j = 0; j < dimension; j++) {
+			tempSum += temp[j] * temp[j];
+		}
+		sum += tempSum * (omegaHo / 2 -2*alpha*alpha);
+	}
+	sum += dimension * numberOfParticles *alpha;
+	return sum;
 }
--- Project1/Code/harmonicocillator.h
+++ Project1/Code/harmonicocillator.h
@@ -6,14 +6,18 @@ class harmonicocillator
 public:
 	harmonicocillator();
 	void setomegaHo(double omegaHo) { this->omegaHo = omegaHo; };
+	void setDimension(double dimension) { this->dimension = dimension; }
 	void setPsi();
 	void setAlpha(double alpha) { this->alpha = alpha; };
+	void setNumberOfParticles(int numberOfParticles) { this->numberOfParticles; }
 private:
+	int numberOfParticles;
 	double alpha;
 	double omegaHo;
+	double dimension;
 	Wavefunction psi;
 	double harmonicOcillatorWavefunction();
-
+	double localEnergy();
 };
 
 harmonicocillator::harmonicocillator()
