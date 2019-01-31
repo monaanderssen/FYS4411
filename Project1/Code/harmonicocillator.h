3d1f7a15cca4319fd459e12ef23b972819425034
jonasron <46788041+jonasron@users.noreply.github.com>
2019-01-31 15:04:23 +0100

    Add files via upload

--- Project1/Code/harmonicocillator.h
+++ Project1/Code/harmonicocillator.h
@@ -10,14 +10,15 @@ public:
 	void setPsi();
 	void setAlpha(double alpha) { this->alpha = alpha; };
 	void setNumberOfParticles(int numberOfParticles) { this->numberOfParticles; }
+	double harmonicOcillatorWavefunction();
+	double localEnergy();
 private:
 	int numberOfParticles;
 	double alpha;
 	double omegaHo;
 	double dimension;
 	Wavefunction psi;
-	double harmonicOcillatorWavefunction();
-	double localEnergy();
+
 };
 
 harmonicocillator::harmonicocillator()
