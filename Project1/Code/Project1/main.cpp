4ee174fa78e3e9fb5b06925fd8870c8918754c7d
Stian Bilek <stianbilek@Stians-MacBook-Pro.local>
2019-02-15 15:38:46 +0100

    fe

--- Project1/Code/Project1/main.cpp
+++ Project1/Code/Project1/main.cpp
@@ -10,12 +10,12 @@ int main()
 {
     SphericalHarmonicOscillator test;
     test.setDimension(3);
-    test.setNumberOfParticles(5);
-    test.setAlpha(0.45);
+    test.setNumberOfParticles(100);
+    test.setAlpha(0.1);
     test.setomegaHo(1);
     test.setPsi();
     Metropolis<SphericalHarmonicOscillator> ttt(test);
-    int iters = 10000;
+    int iters = 100000;
     vec En = ttt.bruteForceSolve(0.1, iters);
     cout<< En(0) << " " << En(1) << endl;
     return 0;
