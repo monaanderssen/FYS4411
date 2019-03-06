ac975586ae0c2a6b8a21ed42be33a9b94c71bb8f
jonasron <46788041+jonasron@users.noreply.github.com>
2019-03-06 15:30:13 +0100

    oo

--- Project1/Code/Project1/harmonicoscillator.cpp
+++ Project1/Code/Project1/harmonicoscillator.cpp
@@ -45,13 +45,10 @@ double HarmonicOscillator::PDF(){
     return x*x;
 }
 
-double HarmonicOscillator::f(int particleOne, int particleTwo){
-    return 0.5;
+double HarmonicOscillator::L1norm(int i, int j){
+    return norm(this->getParticle(i)->getPosition() - this->getParticle(j)->getPosition(), 2);
 }
 
-double HarmonicOscillator::L1norm(int i, int j){
-    return norm(this->getParticle(i)->getPosition() - this->getParticle(j)->getPosition(), 1);
-}
 
 double HarmonicOscillator::V_int(double L1norm){
     if(L1norm > a){
@@ -68,11 +65,108 @@ double HarmonicOscillator::f(double L1norm){
     }
 }
 
-double HarmonicOscillator::AlphaDerivative() {
-	double sum = 0.0;
-	for (int i = 0; i < numberOfParticles; i++) {
-		vec temp = getParticle(i)->getPosition();
-		sum += temp(0)*temp(0) +temp(1)*temp(1) +beta*temp(2)*temp(2);
-	}
-	return -sum;
+double HarmonicOscillator::uDerivative(double L1norm){
+    if(L1norm > a){
+        return a/(L1norm*L1norm - a*L1norm);
+    }
+    else {
+        return 0;
+    }
 }
+
+double HarmonicOscillator::uDoubleDerivative(double L1norm){
+    if(L1norm > a){
+        return (a*a - 2*a*L1norm)/((L1norm*L1norm - a*L1norm)*(L1norm*L1norm - a*L1norm));
+    }
+    else {
+        return 0;
+    }
+}
+
+vec HarmonicOscillator::gradientPhi(int i){
+    vec r = this->getParticle(i)->getPosition();
+    vec temp(3);
+    temp(0) = -2*alpha*(r(0));
+    temp(1) = -2*alpha*(r(1));
+    temp(2) = -2*alpha*(beta*r(2));
+    return temp;
+}
+
+double HarmonicOscillator::doubleGradientPhi(int i){
+    vec r = this->getParticle(i)->getPosition();
+    return -2*alpha*((2 + beta) -2*alpha*(r(0)*r(0) + r(1)*r(1) + beta*r(2)*r(2)));
+}
+
+
+
+double HarmonicOscillator::laplacianPsiOverPsi(int k){
+    double sum = doubleGradientPhi(k);
+    vec r_k = this->getParticle(k)->getPosition();
+    for (int j=0; j <numberOfParticles;j++) {
+        if(j!=k){
+        vec r_j = this->getParticle(j)->getPosition();
+        double norm = L1norm(k,j);
+        sum += 2*dot(gradientPhi(k),r_k-r_j)*uDerivative(norm)/norm;
+        sum += (uDoubleDerivative(norm)+2*uDerivative(norm)/norm);
+        for (int i=0;i<numberOfParticles;i++) {
+            if(i !=k){
+            vec r_i = this->getParticle(i)->getPosition();
+            double norm2 = L1norm(i,k);
+            sum += dot(r_k-r_j,r_k-r_i)*uDerivative(norm)*uDerivative(norm2)/(norm*norm2);
+            }
+        }
+        }
+    }
+}
+
+
+vec HarmonicOscillator::driftForce(int k){
+    vec sum = zeros(3);
+    vec r_k = this->getParticle(k)->getPosition();
+    for (int j=0;j<numberOfParticles;j++) {
+        if(j != k){
+            vec r_j = this->getParticle(j)->getPosition();
+            double norm = L1norm(k,j);
+            sum+= (r_k-r_j)/norm *uDerivative(norm);
+        }
+    }
+    return 2*(sum + gradientPhi(k));
+}
+
+double HarmonicOscillator::psi(){
+    double sum = 0;
+    double sum2 = 1;
+
+    for (int i = 0;i <numberOfParticles;i++) {
+        vec r_i = this->getParticle(i)->getPosition();
+        sum += (r_i(0)*r_i(0) + r_i(1)*r_i(1) + r_i(2)*r_i(2)*beta);
+        for(int j=0;j<i; j++){
+            sum2 *= f(L1norm(i,j));
+        }
+    }
+    return exp(-alpha*sum)*sum2;
+}
+
+double HarmonicOscillator::localEnergy(){
+    double sum = 0;
+    double sum2 = 0;
+
+    for(int i = 0; i<numberOfParticles; i++){
+        vec r_i = this->getParticle(i)->getPosition();
+        sum += r_i(0)*r_i(0) + r_i(1)*r_i(1) + r_i(2)*r_i(2)*gamma - laplacianPsiOverPsi(i);
+        for (int j = 0;j<i;j++) {
+            sum2 += V_int(L1norm(j, i));
+        }
+    }
+    return 0.5*sum + sum2;
+}
+
+
+
+
+
+
+
+
+
+
--- Project1/Code/Project1/harmonicoscillator.h
+++ Project1/Code/Project1/harmonicoscillator.h
@@ -9,14 +9,19 @@ public:
     HarmonicOscillator(int dimensions, int numberOfParticles, double a_, double beta_, double gamma_);
     void set_alpha(double alpha_);
     double g(int particleNumber);
-    double f(int particleOne, int particleTwo);
     double harmonicWavefunction();
     double PDF();
     double L1norm(int i, int j);
     double V_int(double L1norm);
     double f(double L1norm);
-	double AlphaDerivative();
-
+    double uDerivative(double L1norm);
+    double uDoubleDerivative(double L1norm);
+    vec gradientPhi(int i);
+    double doubleGradientPhi(int i);
+    double laplacianPsiOverPsi(int k);
+    vec driftForce(int k);
+    double psi();
+    double localEnergy();
 
 private:
     double a, alpha, beta, gamma;
